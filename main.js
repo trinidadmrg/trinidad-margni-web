d6698dcfd344addcb8ceb8a738943a104c4c6778
Trinidad <89329249+trinidadmrg@users.noreply.github.com>
2024-11-12 13:00:20 -0300

    Updates 2024 V2

---
+++ main.js
@@ -0,0 +1,18 @@
+// Add the 'loaded' class when the page is fully loaded
+window.addEventListener('load', function () {
+    document.documentElement.classList.add('loaded');
+    document.body.classList.add('loaded');
+  });
+  
+// Ensure the page transitions smoothly when navigating away
+  window.addEventListener('beforeunload', function () {
+    document.documentElement.classList.remove('loaded');
+    document.body.classList.remove('loaded');
+  });
+
+// Toggle navbar
+
+  function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('show');
+  }
